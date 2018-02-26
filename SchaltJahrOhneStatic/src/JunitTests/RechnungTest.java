26140a051ce98dde7bb84d4a00960f03ac168b04
Dominic Klein <33520412+4k1r4sf4t3games@users.noreply.github.com>
2018-02-26 12:45:12 +0100

    Add files via upload
    + JUnit Test Case

--- SchaltJahrOhneStatic/src/JunitTests/RechnungTest.java
+++ SchaltJahrOhneStatic/src/JunitTests/RechnungTest.java
@@ -13,6 +13,11 @@ public class RechnungTest{
 	@Test
 	public void testeGeradeZahl() {
 		Rechnung rechnung = new Rechnung();
+		assertTrue(rechnung.isSchaltJahr(4));
+		assertFalse(rechnung.isSchaltJahr(100));
+		assertFalse(rechnung.isSchaltJahr(200));
+		assertTrue(rechnung.isSchaltJahr(400));
+		assertFalse(rechnung.isSchaltJahr(1900));
 		assertTrue(rechnung.isSchaltJahr(2000));
 		assertFalse(rechnung.isSchaltJahr(2001));
 		
--- SchaltJahrOhneStatic/src/com/placeworkers/tools/Rechnung.java
+++ SchaltJahrOhneStatic/src/com/placeworkers/tools/Rechnung.java
@@ -1,13 +1,11 @@
 package com.placeworkers.tools;
 
-
-
-	 public class Rechnung {
-		 public boolean isSchaltJahr(int i) {
-				boolean retVal = false;
-				if ((i % 400 == 0) || (i % 100 != 0) && (i % 4 == 0) ) {
-					retVal = true;
-				}
-				return retVal;
-			}
-			}
+public class Rechnung {
+	public boolean isSchaltJahr(int i) {
+		boolean retVal = false;
+			if (i % 4 == 0 && (i % 100 != 0 || i % 400 == 0)) {
+			retVal = true;
+		}
+		return retVal;
+	}
+}
